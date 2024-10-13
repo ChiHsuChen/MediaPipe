c6d92998c1a3ef75a8b6b67710c9a12d377032cf
css920 <css920@gmail.com>
2024-10-13 16:22:16 +0800

    新增function

--- Preprocessing/image_preprocessing.py
+++ Preprocessing/image_preprocessing.py
@@ -5,10 +5,18 @@ import numpy as np
 def gamma_correction(image, gamma):
     if image is None:
         raise ValueError("The provided image is invalid.")
-
     # Apply gamma correction
     inv_gamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in range(256)]).astype("uint8")
     gamma_corrected = cv2.LUT(image, table)
-
     return gamma_corrected
+
+def open_image(image, kernel_size=(5, 5)):
+    kernel = np.ones(kernel_size, np.uint8)
+    opened_image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    return opened_image
+
+def close_image(image, kernel_size=(5, 5)):
+    kernel = np.ones(kernel_size, np.uint8)
+    closed_image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    return closed_image
